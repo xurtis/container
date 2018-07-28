5fe4e07d2c750d74555973167dc803b830a53683
Curtis Millar <curtis@curtism.me>
2018-07-28 22:28:50 +1000

    Add user mappings.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 #[macro_use]
 extern crate error_chain;
+extern crate libc;
 extern crate loadconf;
 extern crate nix;
 extern crate serde;
@@ -18,7 +19,10 @@ mod error;
 use std::env;
 use std::ffi::{OsStr, OsString};
 use std::process;
+use std::path::{Path, PathBuf};
+use std::os::unix::fs::PermissionsExt;
 
+use libc::{uid_t, gid_t};
 use loadconf::Load;
 use unshare::ExitStatus;
 
@@ -35,7 +39,7 @@ const DEFAULT_COMMAND: &'static str = "/bin/sh";
 
 /// Serialisable namespaces.
 #[derive(Debug, Clone, Copy, Deserialize)]
-#[serde(rename_all = "lowercase")]
+#[serde(rename_all = "snake_case")]
 enum Namespace {
     Mount,
     Uts,
@@ -60,26 +64,81 @@ impl Into<unshare::Namespace> for Namespace {
     }
 }
 
-impl<'a> Into<unshare::Namespace> for &'a Namespace {
-    fn into(self) -> unshare::Namespace {
-        self.clone().into()
+#[derive(Debug, Deserialize)]
+struct UidMap {
+    inside: uid_t,
+    outside: uid_t,
+    count: uid_t,
+}
+
+impl Into<unshare::UidMap> for UidMap {
+    fn into(self) -> unshare::UidMap {
+        let UidMap {inside, outside, count} = self;
+        unshare::UidMap {
+            inside_uid: inside,
+            outside_uid: outside,
+            count
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct GidMap {
+    inside: gid_t,
+    outside: gid_t,
+    count: gid_t,
+}
+
+impl Into<unshare::GidMap> for GidMap {
+    fn into(self) -> unshare::GidMap {
+        let GidMap {inside, outside, count} = self;
+        unshare::GidMap {
+            inside_gid: inside,
+            outside_gid: outside,
+            count
+        }
     }
 }
 
 /// Configuration for the container.
 #[derive(Debug, Default, Deserialize)]
 struct Config {
+    #[serde(default)]
     namespaces: Vec<Namespace>,
+    #[serde(default)]
+    uid: uid_t,
+    #[serde(default)]
+    gid: gid_t,
+    #[serde(default)]
+    uid_map: Vec<UidMap>,
+    #[serde(default)]
+    gid_map: Vec<GidMap>,
 }
 
 impl Config {
     /// Configure the container prior to the container.
-    fn unshare(&self, command: &mut unshare::Command) {
-        command.unshare(self.namespaces.iter().map(Into::into));
+    fn unshare(self, command: &mut unshare::Command) {
+        let Config { namespaces, uid_map, gid_map, uid, gid } = self;
+        command.unshare(namespaces.into_iter().map(Namespace::into));
+        command.set_id_maps(
+            uid_map.into_iter().map(UidMap::into).collect(),
+            gid_map.into_iter().map(GidMap::into).collect(),
+        );
+
+        if let (
+            Some(newuidmap), Some(newgidmap)
+        ) = (
+            find_exec("newuidmap"), find_exec("newgidmap")
+        ) {
+            command.set_id_map_commands(newuidmap, newgidmap);
+        }
+
+        command.uid(uid);
+        command.gid(gid);
     }
 
     /// Configure the container after having entered.
-    fn configure(&self, _command: &mut process::Command) {
+    fn configure(self, _command: &mut process::Command) {
     }
 }
 
@@ -139,4 +198,29 @@ fn child_command() -> Vec<OsString> {
     } else {
         args
     }
+}
+
+/// Find a path for an executable.
+fn find_exec<P: AsRef<Path>>(executable: P) -> Option<PathBuf> {
+    env::var_os("PATH")
+        .as_ref()
+        .and_then(|s| s.to_str())
+        .and_then(|s| Some(s.split(':')))
+        .and_then(|p| find_first(p, executable))
+}
+
+fn find_first<'p, I, P, E>(paths: I, executable: E) -> Option<PathBuf>
+where
+    I: Iterator<Item = P>,
+    P: AsRef<Path>,
+    E: AsRef<Path>,
+{
+    for prefix in paths {
+        let path = prefix.as_ref().join(executable.as_ref());
+        if path.exists() {
+            return Some(path)
+        }
+    }
+
+    None
 }
